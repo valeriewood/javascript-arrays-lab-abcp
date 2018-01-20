f16066a5453a3bd19779d997cbc98643d708d189
Valerie Wood <valeriewd1@gmail.com>
2018-01-20 03:58:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+ var kittens = ["Milo", "Otis", "Garfield"];
+ 
+ var destructivelyAppendKitten = ["Milo", "Otis", "Garfield"];
+ destructivelyAppendKitten.push("Ralph");
+ destructivelyAppendKitten;
+
+  
+  
+var destructivelyPrependKitten = ["Milo", "Otis", "Garfield", "Ralph"];
+ destructivelyPrependKitten.unshift("Bob");
+ destructivelyPrependKitten;
+  
+var destructivelyRemoveLastKitten = ["Bob","Milo", "Otis", "Garfield", "Ralph"];
+ destructivelyRemoveLastKitten.pop();
+  destructivelyRemoveLastKitten;
+
+     
